24a5618c63f5338daac197fbc4d75f38ffd93aa6
Nicolás Celedón Canales <nicolas.celedon@zembia.cl>
2023-05-30 12:46:54 -0400

    feat: Update setup file to includes dependencies

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     description="API para plataforma Pulse de Zembia",
     author="Zembia SpA"
     packages=["pulse_api"],
+    install_requires=["PyInquirer>=1.0.3","requests>=2.25.1"],
+    python_requires='>=3.7.9',
     zip_safe=False,
 )
+
+
   
