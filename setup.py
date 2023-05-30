46806d42a116f6011e5297270eec54dbdde0a4fa
Nicolás Celedón Canales <nicolas.celedon@zembia.cl>
2023-05-30 15:05:33 -0400

    fix: Add comma on setup.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="your_module",
+    name="pulse_api",
     version="0.0.1",
     description="API para plataforma Pulse de Zembia",
-    author="Zembia SpA"
+    author="Zembia SpA",
     packages=["pulse_api"],
-    install_requires=["PyInquirer>=1.0.3","requests>=2.25.1"],
-    python_requires='>=3.7.9',
+    install_requires=["PyInquirer>=1.0.3", "requests>=2.25.1"],
+    python_requires=">=3.7.9",
     zip_safe=False,
 )
-
-
-  
