5f29b1699ebcaac36ac2a776cf246794c0d4e6fa
Nicolás Celedón Canales <nicolas.celedon@zembia.cl>
2023-06-05 12:24:59 -0400

    feat: request with threading request

--- pulse_api/pulse_api.py
+++ pulse_api/pulse_api.py
@@ -1,6 +1,15 @@
+from threading import Thread, Lock
 from PyInquirer import prompt
 from dateutil.parser import parse
 import requests
+from time import sleep
+import urllib3
+
+REQUESTS_LIMIT = 20
+thread_lock = Lock()
+running_threads = []
+request_threads = []
+data_threads = []
 
 
 class PulseAPI:
@@ -64,10 +73,10 @@ class PulseAPI:
 
         return authorization
 
-    def get_measure_names(self, authorization, device_id):
+    def get_measure_names(self, authorization, device_id, verify=True):
         url = f"{self.backend_url}/devices/{device_id}/measure_names.json"
         headers = {"Authorization": authorization}
-        res = requests.get(url=url, headers=headers)
+        res = requests.get(url=url, headers=headers, verify=verify)
         res_json = res.json()
         return res_json
 
@@ -82,28 +91,65 @@ class PulseAPI:
     def get_measures(
         self,
         authorization,
-        device_id,
+        device_id_list,
         start_date,
         end_date,
         measure_name_id_list,
         verify=True,
     ):
+        # Parse dates
         start_date = parse(start_date, fuzzy=True)
         end_date = parse(end_date, fuzzy=True)
 
+        # Request params
         params = {
             "start_date": start_date,
             "end_date": end_date,
             "order": "origin_created_at",
             "measure_name_id[]": measure_name_id_list,
         }
-
-        url = (
-            f"{self.backend_url}/devices/{device_id}/measures/faster_simple_graph.json"
-        )
         headers = {"Authorization": authorization}
 
+        if type(device_id_list) is list:
+            for thread_index, device_id in enumerate(device_id_list):
+                url = f"{self.backend_url}/devices/{device_id}/measures/faster_simple_graph.json"
+                args = [thread_index, params, headers, url, verify]
+                running_threads.append(Thread(target=self.thread_request, args=args))
+                running_threads[-1].start()
+        else:
+            url = f"{self.backend_url}/devices/{device_id}/measures/faster_simple_graph.json"
+
+            res = requests.get(url=url, params=params, headers=headers, verify=verify)
+            res_json = res.json()
+
+            return res_json
+
+    def thread_request(self, thread_index, params, headers, url, verify):
+        global thread_lock, request_threads, data_threads
+
+        # Acquire request slot
+        while 1:
+            thread_lock.acquire()
+            if len(request_threads) >= REQUESTS_LIMIT:                
+                thread_lock.release()
+                sleep(0.2)
+                continue
+
+            request_threads.append(thread_index)
+            thread_lock.release()
+            break
+
+        # Make request
         res = requests.get(url=url, params=params, headers=headers, verify=verify)
         res_json = res.json()
 
-        return res_json
+        # Release request slot
+        thread_lock.acquire()
+        i = request_threads.index(thread_index)
+        del request_threads[i]
+        thread_lock.release()
+
+        # Append data
+        thread_lock.acquire()
+        data_threads.append(res_json)
+        thread_lock.release()
