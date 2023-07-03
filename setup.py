c4dacc4d1f0ab676c4719773e525404506b79a25
Nicolás Celedón Canales <71094200+nicoceledonc@users.noreply.github.com>
2023-07-03 10:34:48 -0400

    Update setup.py
    Update python version

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author="Zembia SpA",
     packages=["pulse_api"],
     install_requires=["PyInquirer>=1.0.3", "requests>=2.25.1"],
-    python_requires=">=3.7.9",
+    python_requires=">=3.7.5",
     zip_safe=False,
 )
